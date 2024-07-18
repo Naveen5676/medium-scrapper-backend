eed0b8e3c025bef58e19fe5b85be6f0b46ae1a1b
Naveen5676 <sample.first@yahoo.com>
2024-07-18 12:24:10 +0530

    updated args and executable for puppeteer launch

--- Controllers/ScrapeMedium.js
+++ Controllers/ScrapeMedium.js
@@ -1,7 +1,16 @@
 const puppeteer = require("puppeteer");
+require('dotenv').config();
 
 async function scrapeMedium(topic) {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ 
+    args:[
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath: process.env.NODE_ENV === "production" ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
+    headless: true });
   let articles = [];
 
   try {
